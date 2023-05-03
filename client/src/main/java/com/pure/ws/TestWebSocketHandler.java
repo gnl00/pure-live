f9ead2b522c1995c5aac796b303d8b6b9b471918
gnl <gnl0@qq.com>
2023-05-03 23:47:56 +0800

    udpate

--- client/src/main/java/com/pure/ws/TestWebSocketHandler.java
+++ client/src/main/java/com/pure/ws/TestWebSocketHandler.java
@@ -8,6 +8,8 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -21,22 +23,25 @@ import java.util.Objects;
 @ServerEndpoint("/ws/test/{receiver}")
 public class TestWebSocketHandler {
 
-    static Session latestSession;
+    static List<Session> receivers = new ArrayList<>();
 
     @OnOpen
     public void onOpen(Session session, @PathParam("receiver") String param) {
 
         if (Objects.nonNull(param) && param.equals("1")) {
-            log.info(param);
-            this.latestSession = session;
+            log.info("receiver");
+            receivers.add(session);
+            log.info("receivers size {}", receivers.size());
         }
 
         log.info("new link join");
     }
 
     @OnClose
-    public void onClose(CloseReason reason) {
+    public void onClose(Session session, CloseReason reason) {
         log.info("One link closed, reason: {}", reason.getReasonPhrase());
+        receivers.remove(session);
+        log.info("receivers size {}", receivers.size());
     }
 
     @OnError
@@ -44,11 +49,19 @@ public class TestWebSocketHandler {
         t.printStackTrace();
     }
 
-    @OnMessage
-    public void onMessage(ByteBuffer buffer) throws IOException {
+    // 默认支持的最大消息为 8192=8kb
+    @OnMessage(maxMessageSize = 5120000)
+    public void onMessage(ByteBuffer buffer) {
         log.info("[received]: {}", buffer);
 
-        latestSession.getBasicRemote().sendBinary(buffer);
+        receivers.forEach(receiver -> {
+            try {
+                receiver.getBasicRemote().sendBinary(buffer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
     }
 
     public void sendData() {}
