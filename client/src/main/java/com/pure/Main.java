8cd8a347a6940f7a6c851e85c7ce274f1ec23b5f
gnl <gnl0@qq.com>
2023-05-05 20:29:50 +0800

    update

--- client/src/main/java/com/pure/Main.java
+++ client/src/main/java/com/pure/Main.java
@@ -1,9 +1,13 @@
 package com.pure;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import org.bytedeco.ffmpeg.global.avcodec;
+import org.bytedeco.ffmpeg.global.avutil;
+import org.bytedeco.javacv.*;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * TODO
@@ -13,13 +17,87 @@ import java.nio.file.Path;
  */
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        ProcessBuilder pb = new ProcessBuilder("ffmpeg", "-i", "input.mp4", "-c:v", "libx264", "-hls_time", "10", "-hls_list_size", "0", "-hls_segment_filename", "output_%d.ts", "output.m3u8");
+        // mp4ToM3u8();
+
+        FileInputStream  fis = new FileInputStream("test.mp4");
+        byte[] videoBytes = fis.readAllBytes();
+        ByteBuffer videoBuffer = ByteBuffer.wrap(videoBytes);
+        fis.close();
+
+        bytesToM3u8(videoBuffer);
+    }
+
+
+     // javacv + ffmpeg-platform
+     public static void bytesToM3u8(ByteBuffer buffer) throws FrameGrabber.Exception, FrameRecorder.Exception {
+        File tmpFile = new File("tmp");
+
+        try (FileOutputStream fos = new FileOutputStream(tmpFile)) {
+            fos.write(buffer.array());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 设置 FFmpeg 的配置信息
+        FFmpegFrameGrabber fg = new FFmpegFrameGrabber(tmpFile);
+        Map<String, String> options = new HashMap<>();
+        options.put("-y", "-"); // 覆盖输出文件
+        options.put("-threads", "8"); // 线程数
+        options.put("-i", "-"); // 从标准输入流读入
+        options.put("-c:v", "libx264");
+        options.put("-profile:v", "high");
+        options.put("-level", "3.1");
+        options.put("-preset", "slow"); // 编码质量为最慢
+        options.put("-crf", "28"); // 编码质量为 28
+        options.put("-sc_threshold", "0"); // 不过滤场景切换
+        options.put("-g", "25");
+        // options.put("-hls_time", "10"); // 10 秒一个分片
+        options.put("-hls_list_size", "0"); // 不限制 m3u8 列表文件的大小
+        options.put("-start_number", "0"); // 分片开始的序号
+        options.put("-hls_segment_filename", "%03d.ts"); // ts 分片保存目录和文件名格式
+        options.put("output.m3u8", "-"); // m3u8 文件
+        fg.setOptions(options);
+
+        // 初始化 FFmpeg
+        fg.start();
+
+        // 创建一个 FFmpeg 视频编码器
+        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder("output.m3u8", fg.getImageWidth(), fg.getImageHeight(), fg.getAudioChannels());
+        recorder.setAudioCodec(avcodec.AV_CODEC_ID_AAC);
+        recorder.setVideoOption("preset", "slow");
+        recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+        recorder.setPixelFormat(avutil.AV_PIX_FMT_YUV420P);
+        recorder.setFormat("hls");
+        recorder.setVideoBitrate(900 * 1024);
+        recorder.setAudioBitrate(128 * 1024);
+        recorder.setFrameRate(fg.getFrameRate());
+        recorder.setVideoQuality(26);
+        recorder.setGopSize((int)Math.round(fg.getFrameRate()));
+
+        // 初始化编码器
+        recorder.start();
+
+        // 每一帧图像进行编码并写入到 FFmpeg 中，直到读取完所有帧
+        Frame frame = null;
+        int count = 0;
+        while ((frame = fg.grab()) != null) {
+            recorder.setTimestamp(fg.getTimestamp());
+            recorder.record(frame);
+            count++;
+        }
+
+        System.out.println("Total number of frames: " + count);
+
+        // release
+        recorder.close();
+        fg.close();
+
+        tmpFile.delete();
+     }
+
+    public static void mp4ToM3u8() throws IOException, InterruptedException {
+        ProcessBuilder pb = new ProcessBuilder("ffmpeg", "-i", "test.mp4", "-c:v", "libx264", "-hls_time", "10", "-hls_list_size", "0", "-hls_segment_filename", "output_%d.ts", "output.m3u8");
         Process process = pb.start();
         process.waitFor();
-
-//        byte[] m3u8Data = Files.readAllBytes(Path.of("output.m3u8"));
-//        try (FileOutputStream fos = new FileOutputStream("foutput.m3u8")) {
-//            fos.write(m3u8Data);
-//        }
     }
 }
--- client/src/main/java/com/pure/ws/TestWebSocketHandler.java
+++ client/src/main/java/com/pure/ws/TestWebSocketHandler.java
@@ -1,12 +1,18 @@
 package com.pure.ws;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.bytedeco.ffmpeg.global.avcodec;
+import org.bytedeco.ffmpeg.global.avutil;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FFmpegFrameRecorder;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -22,6 +28,8 @@ import java.util.*;
 @Component
 @ServerEndpoint("/ws/test/{receiver}")
 public class TestWebSocketHandler {
+
+    final static String TMP_DIR = System.getProperty("java.io.tmpdir");
 
     static List<Session> receivers = new ArrayList<>();
 
@@ -54,30 +62,70 @@ public class TestWebSocketHandler {
     public void onMessage(ByteBuffer buffer) {
         log.info("[received]: {}", buffer);
 
-        saveVideoBytes(buffer);
+        String mLink = bytesToM3u8(buffer);
+
+//        String fileName = saveVideoBytes(buffer);
+//        String link = getLink(fileName);
 
         receivers.forEach(receiver -> {
             try {
-                receiver.getBasicRemote().sendBinary(buffer);
-                // receiver.getBasicRemote().sendText(handleVideoBytes(buffer));
+                // receiver.getBasicRemote().sendBinary(buffer);
+                receiver.getBasicRemote().sendText(mLink);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         });
     }
 
-    private void saveVideoBytes(ByteBuffer buffer) {
-        String filename = generateMp4(buffer);
-        convertMp4ToM3u8(filename);
+    private String bytesToM3u8(ByteBuffer buffer) {
+        File tmpFile = new File("tmp");
+
+        try (FileOutputStream fos = new FileOutputStream(tmpFile)) {
+            fos.write(buffer.array());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FFmpegFrameGrabber fg = new FFmpegFrameGrabber(tmpFile);
+
+        // 创建一个 FFmpeg 视频编码器
+        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(tmpFile, fg.getImageWidth(), fg.getImageHeight(), fg.getAudioChannels());
+        recorder.setAudioCodec(avcodec.AV_CODEC_ID_AAC);
+        recorder.setVideoOption("preset", "veryfast");
+        recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+        recorder.setPixelFormat(avutil.AV_PIX_FMT_YUV420P);
+        recorder.setFormat("hls");
+        recorder.setVideoBitrate(900 * 1024);
+        recorder.setAudioBitrate(128 * 1024);
+        recorder.setFrameRate(fg.getFrameRate());
+        recorder.setVideoQuality(26);
+        recorder.setGopSize((int)Math.round(fg.getFrameRate()));
+
+
+        tmpFile.delete();
+
+        return null;
     }
 
-    private void convertMp4ToM3u8(String filename) {
+    private String getLink(String fileName) {
+        return "http://localhost:8888/video/" + fileName + ".m3u8";
+    }
+
+    private String saveVideoBytes(ByteBuffer buffer) {
+        String mp4FileName = generateMp4(buffer);
+        String m3FileName = convertMp4ToM3u8(mp4FileName);
+
+        return m3FileName;
     }
 
     public String generateMp4(ByteBuffer buffer) {
-        String filename = new Date().getTime() + ".mp4";
+        String filePath = "/Users/gnl/Tmp/statics/video/";
+        String outputSuffix =  ".mp4";
+        String filename = new Date().getTime() + "";
+        String outputFileName = filePath + filename + outputSuffix;
+
         byte[] videoBytes = buffer.array();
-        try (FileOutputStream fos = new FileOutputStream(filename + ".mp4")) {
+        try (FileOutputStream fos = new FileOutputStream(outputFileName)) {
             fos.write(videoBytes);
 
         } catch (IOException e) {
@@ -85,6 +133,31 @@ public class TestWebSocketHandler {
         }
 
         return filename;
+    }
+
+    public String convertMp4ToM3u8(String filename) {
+        try {
+            String filePath = "/Users/gnl/Tmp/statics/video/";
+            String inputSuffix =  ".mp4";
+            String inputFileName = filePath + filename + inputSuffix;
+
+            String m3Suffix = ".m3u8";
+            String tsSuffix = ".ts";
+
+            String outputM3u8 = filePath + filename + m3Suffix;
+            // String outputTs = filePath + filename + "_%d" + tsSuffix; // "output_%d.ts"
+
+            // ffmpeg -i input.mp4 -codec copy -hls_list_size 0 output.m3u8
+            ProcessBuilder pb = new ProcessBuilder("ffmpeg", "-i", inputFileName, "-codec", "copy", "-hls_list_size", "0", outputM3u8);
+            Process process = pb.start();
+            process.waitFor();
+
+            return filename;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String handleVideoBytes(ByteBuffer buffer) {
@@ -131,7 +204,5 @@ public class TestWebSocketHandler {
 
         return builder.toString();
     }
-
-    public void sendData() {}
 
 }
